0cb4876776f4cbc0cd3c40d8daef46a122497575
7aeHoon <108669890+7aeHoon@users.noreply.github.com>
2025-12-01 18:23:09 +0900

    백준 1940번 - 주몽
    Implement two-pointer technique to count pairs summing to m.

---
+++ 자료구조/투 포인터/주몽.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> v;
+    int n, m, count = 0;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (const auto& value : v) {
+        if (binary_search(v.begin(), v.end(), m - value)) {
+            count++;
+        }
+    }
+
+    cout << count / 2  << '\n';
+
+    return 0;
+}
